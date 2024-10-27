9e8ce60fc5925052c0bb81f41d1634db3065fb88
11121997 <nawalkhaled27@yahoo.com>
2024-10-27 22:04:32 +0300

    main

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import LoginRegister from "./components/loginRegister/loginRegister";
+
+function App() {
+  return (
+    <div>
+      <LoginRegister />
+    </div>
+  );
+};
+
+export default App;
