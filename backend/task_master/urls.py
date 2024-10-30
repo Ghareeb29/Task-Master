ad43c8b01f866f6d74b28c281743586cbc326cb0
ibrahim <irahimammar811@gmail.com>
2024-10-30 16:30:46 +0300

    Add API documentation using drf-spectacular

--- backend/task_master/urls.py
+++ backend/task_master/urls.py
@@ -28,18 +28,11 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', UserProfileView.as_view(), name='user_profile'),
-<<<<<<< HEAD
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('tasks.urls')),
-   
-=======
-    
     # Adding API documentation routes using drf-spectacular
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # Raw OpenAPI schema
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI interface
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # ReDoc interface
-
-    
->>>>>>> 184c1c6 (Add API documentation using drf-spectacular)
 ]
